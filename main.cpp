74d318e7f272940a0e471f4095621a3eeaf6f616
Esteban de la Maza <A01720548@itesm.mx>
2021-08-16 15:46:33 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+// g++ main.cpp -o main.exe
+// main.exe < in.txt
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<double> vec;
+
+vector<int> mergeSort(vector<int> n)
+{
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    double num;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> num;
+        vec.push_back(num);
+    }
+}
